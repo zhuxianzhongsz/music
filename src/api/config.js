6b0a7bd140acd465cc2bb5b75a1d11fc119eeb23
zhuxianzhong <22525482@qq.com>
2018-12-08 14:21:17 +0800

    music

---
+++ src/api/config.js
@@ -0,0 +1,15 @@
+// url通用参数
+export const commonParams = {
+  g_tk: 1928093487,
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  notice: 0,
+  format: 'jsonp'
+}
+// jsonp参数
+export const options = {
+  param: 'jsonpCallback',
+  prefix: 'jp'
+}
+
+export const ERR_OK = 0
